ff27324e9a1918d3676d356b17a5b4d95a6ecebe
PabloMata7 <50667442+PabloMata7@users.noreply.github.com>
2026-02-13 09:50:25 +0100

    CreacionXMLs

--- addons/pokemon/__manifest__.py
+++ addons/pokemon/__manifest__.py
@@ -22,13 +22,14 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
         'views/templates.xml',
+        'views/pokemon_menu.xml'
+        'views/pokemon_views.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
+    'application': True,
+    'installable': True,
 }
 
