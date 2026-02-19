ca582c6aa3dc1a1dece79ec194ffdf2f0879f8cf
PabloMata7 <50667442+PabloMata7@users.noreply.github.com>
2026-02-19 12:21:07 +0100

    Final

--- addons/lorcana/__manifest__.py
+++ addons/lorcana/__manifest__.py
@@ -2,14 +2,9 @@
 {
     'name': "Lorcana",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Modulo de Lorcana",
 
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Jorge Diaz",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -18,17 +13,18 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
         'views/templates.xml',
+        'views/lorcana_views.xml',
+        'views/lorcana_menu.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
+    'application': True,
+    'installable': True,
 }
 
