daec48de3ede5a53107d78a6e0a321ddc6f9c325
PabloMata7 <50667442+PabloMata7@users.noreply.github.com>
2026-02-19 11:48:22 +0100

    ajuste one piece

--- addons/one_piece/controllers/controllers.py
+++ addons/one_piece/controllers/controllers.py
@@ -4,12 +4,12 @@ from odoo.http import request
 
 class OnePieceWebsite(http.Controller):
 
-    @http.route(['/onepiece'], type='http', auth='public', website=True)
+    @http.route(['/one_piece'], type='http', auth='public', website=True)
     def index(self, **kw):
         nakamas = request.env['onepiece.card'].sudo().search([])
         return request.render("one_piece.website_list", {'nakamas': nakamas})
 
-    @http.route(['/onepiece/<int:card_id>'], type='http', auth='public', website=True)
+    @http.route(['/one_piece/<int:card_id>'], type='http', auth='public', website=True)
     def detail(self, card_id, **kw):
         card = request.env['onepiece.card'].sudo().browse(card_id)
         return request.render("one_piece.website_detail", {'card': card})
